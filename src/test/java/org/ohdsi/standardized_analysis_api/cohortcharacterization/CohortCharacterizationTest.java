9ff89936ed455acd2c40b8ebf713154201eb6b80
PGrafkin <pavgra@gmail.com>
2018-07-26 16:43:41 -0400

    Added separate CriteriaFeature class

--- src/test/java/org/ohdsi/standardized_analysis_api/cohortcharacterization/CohortCharacterizationTest.java
+++ src/test/java/org/ohdsi/standardized_analysis_api/cohortcharacterization/CohortCharacterizationTest.java
@@ -9,7 +9,6 @@ import org.ohdsi.circe.cohortdefinition.Criteria;
 import org.ohdsi.circe.cohortdefinition.CriteriaGroup;
 import org.ohdsi.circe.cohortdefinition.DemographicCriteria;
 import org.ohdsi.circe.cohortdefinition.DrugExposure;
-import org.ohdsi.circe.cohortdefinition.InclusionRule;
 import org.ohdsi.circe.cohortdefinition.ObservationFilter;
 import org.ohdsi.circe.cohortdefinition.Period;
 import org.ohdsi.circe.cohortdefinition.PrimaryCriteria;
@@ -19,6 +18,7 @@ import org.ohdsi.circe.vocabulary.ConceptSetExpression;
 import org.ohdsi.standardized_analysis_api.Utils;
 import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.CohortCharacterization;
 import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.CohortCharacterizationParam;
+import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.CriteriaFeature;
 import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.FeatureAnalysis;
 import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.FeatureAnalysisDomain;
 import org.ohdsi.standardized_analysis_api.cohortcharacterization.design.FeatureAnalysisType;
@@ -139,55 +139,71 @@ public class CohortCharacterizationTest extends BaseTest {
                         return "Gender of the subject via criteria.";
                     }
 
-                    public List<InclusionRule> getDesign() {
+                    public List<CriteriaFeature> getDesign() {
 
-                        Concept maleConcept = new Concept();
-                        maleConcept.conceptCode = "M";
-                        maleConcept.conceptId = 8507L;
-                        maleConcept.conceptName = "MALE";
-                        maleConcept.domainId = "Gender";
-                        maleConcept.vocabularyId = "Gender";
-                        maleConcept.standardConcept = "S";
-                        maleConcept.invalidReason = "V";
-                        maleConcept.conceptClassId = "Gender";
+                        CriteriaFeature maleCriteria = new CriteriaFeature() {
 
-                        DemographicCriteria demographicMaleCriteria = new DemographicCriteria();
-                        demographicMaleCriteria.gender = new Concept[] { maleConcept };
+                            public String getName() {
 
-                        CriteriaGroup maleGroup = new CriteriaGroup();
-                        maleGroup.type = "ALL";
-                        maleGroup.criteriaList = new CorelatedCriteria[] {};
-                        maleGroup.demographicCriteriaList = new DemographicCriteria[] { demographicMaleCriteria };
+                                return "Male";
+                            }
 
-                        InclusionRule maleInclusionRule = new InclusionRule();
-                        maleInclusionRule.name = "Male";
-                        maleInclusionRule.expression = maleGroup;
+                            public CriteriaGroup getCriteriaGroup() {
 
-                        //
+                                Concept maleConcept = new Concept();
+                                maleConcept.conceptCode = "M";
+                                maleConcept.conceptId = 8507L;
+                                maleConcept.conceptName = "MALE";
+                                maleConcept.domainId = "Gender";
+                                maleConcept.vocabularyId = "Gender";
+                                maleConcept.standardConcept = "S";
+                                maleConcept.invalidReason = "V";
+                                maleConcept.conceptClassId = "Gender";
 
-                        Concept femaleConcept = new Concept();
-                        femaleConcept.conceptCode = "F";
-                        femaleConcept.conceptId = 8532L;
-                        femaleConcept.conceptName = "FEMALE";
-                        femaleConcept.domainId = "Gender";
-                        femaleConcept.vocabularyId = "Gender";
-                        femaleConcept.standardConcept = "S";
-                        femaleConcept.invalidReason = "V";
-                        femaleConcept.conceptClassId = "Gender";
+                                DemographicCriteria demographicMaleCriteria = new DemographicCriteria();
+                                demographicMaleCriteria.gender = new Concept[] { maleConcept };
 
-                        DemographicCriteria demographicFemaleCriteria = new DemographicCriteria();
-                        demographicFemaleCriteria.gender = new Concept[] { femaleConcept };
+                                CriteriaGroup maleGroup = new CriteriaGroup();
+                                maleGroup.type = "ALL";
+                                maleGroup.criteriaList = new CorelatedCriteria[] {};
+                                maleGroup.demographicCriteriaList = new DemographicCriteria[] { demographicMaleCriteria };
 
-                        CriteriaGroup femaleGroup = new CriteriaGroup();
-                        femaleGroup.type = "ALL";
-                        femaleGroup.criteriaList = new CorelatedCriteria[] {};
-                        femaleGroup.demographicCriteriaList = new DemographicCriteria[] { demographicFemaleCriteria };
+                                return maleGroup;
+                            }
+                        };
 
-                        InclusionRule femaleInclusionRule = new InclusionRule();
-                        femaleInclusionRule.name = "Female";
-                        femaleInclusionRule.expression = femaleGroup;
+                        CriteriaFeature femaleCriteria = new CriteriaFeature() {
 
-                        return Arrays.asList(maleInclusionRule, femaleInclusionRule);
+                            public String getName() {
+
+                                return "Female";
+                            }
+
+                            public CriteriaGroup getCriteriaGroup() {
+
+                                Concept femaleConcept = new Concept();
+                                femaleConcept.conceptCode = "F";
+                                femaleConcept.conceptId = 8532L;
+                                femaleConcept.conceptName = "FEMALE";
+                                femaleConcept.domainId = "Gender";
+                                femaleConcept.vocabularyId = "Gender";
+                                femaleConcept.standardConcept = "S";
+                                femaleConcept.invalidReason = "V";
+                                femaleConcept.conceptClassId = "Gender";
+
+                                DemographicCriteria demographicFemaleCriteria = new DemographicCriteria();
+                                demographicFemaleCriteria.gender = new Concept[] { femaleConcept };
+
+                                CriteriaGroup femaleGroup = new CriteriaGroup();
+                                femaleGroup.type = "ALL";
+                                femaleGroup.criteriaList = new CorelatedCriteria[] {};
+                                femaleGroup.demographicCriteriaList = new DemographicCriteria[] { demographicFemaleCriteria };
+
+                                return femaleGroup;
+                            }
+                        };
+
+                        return Arrays.asList(maleCriteria, femaleCriteria);
                     }
                 };
 
