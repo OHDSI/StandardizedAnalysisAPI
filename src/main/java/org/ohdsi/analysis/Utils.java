e44c17a871401c1a1d0f871e6bb8940d6df072a9
PGrafkin <pavgra@gmail.com>
2018-10-05 14:00:05 -0400

    Introduced new deserialization methods

--- src/main/java/org/ohdsi/analysis/Utils.java
+++ src/main/java/org/ohdsi/analysis/Utils.java
@@ -1,16 +1,21 @@
 package org.ohdsi.analysis;
 
+import static com.fasterxml.jackson.core.util.DefaultIndenter.SYSTEM_LINEFEED_INSTANCE;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.core.util.Separators;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-
-import static com.fasterxml.jackson.core.util.DefaultIndenter.SYSTEM_LINEFEED_INSTANCE;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class Utils {
 
@@ -68,6 +73,44 @@ public class Utils {
         ObjectMapper objectMapper = getObjectMapper();
         try {
             return objectMapper.readValue(data, typeRef);
+        } catch (NullPointerException ex) {
+            return null;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static <T> T deserialize(String data, JavaType typeRef) {
+        ObjectMapper objectMapper = getObjectMapper();
+        try {
+            return objectMapper.readValue(data, typeRef);
+        } catch (NullPointerException ex) {
+            return null;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static <T> T deserialize(String data, Function<TypeFactory, JavaType> typeFunction) {
+
+        Objects.requireNonNull(typeFunction);
+        ObjectMapper objectMapper = getObjectMapper();
+        JavaType type = typeFunction.apply(objectMapper.getTypeFactory());
+        try{
+            return objectMapper.readValue(data, type);
+        }catch (NullPointerException ex) {
+            return null;
+        }catch(Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static <T> T deserialize(String data, Class<T> objectClass) {
+
+        Objects.requireNonNull(objectClass);
+        ObjectMapper objectMapper = getObjectMapper();
+        try {
+            return objectMapper.readValue(data, objectClass);
         } catch (NullPointerException ex) {
             return null;
         } catch (Exception ex) {
