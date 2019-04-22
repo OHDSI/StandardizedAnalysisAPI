d1a81d3057c4fa766d4de821ac5465224eccf6f2
PGrafkin <pavgra@gmail.com>
2019-04-22 13:42:20 -0400

    Fixed tests

--- standardized-analysis-utils/src/test/java/org/ohdsi/analysis/UtilsTest.java
+++ standardized-analysis-utils/src/test/java/org/ohdsi/analysis/UtilsTest.java
@@ -12,23 +12,28 @@ import static org.junit.Assert.assertEquals;
 
 public class UtilsTest extends BaseTest {
 
+    private static final SimpleModule CUSTOM_TYPE_MODULE = new SimpleModule().registerSubtypes(
+            new NamedType(org.ohdsi.analysis.pojos.custom.SampleJson.class, "SampleJson")
+    );
+
     @Test
     public void serializeDefaultWithoutNulls() throws IOException {
 
-        String serialized = Utils.serialize(SampleJson.newInstance().withName("SampleJson").withCount(123));
+        String serialized = Utils.serialize(SampleJson.newInstance().withName("Sample").withCount(123));
         assertEquals(readResource("/sample.json"), serialized);
     }
 
     @Test
     public void serializeDefaultWithNulls() throws IOException {
 
-        String serialized = Utils.serialize(SampleJson.newInstance().withName("SampleJson").withCount(123), true);
+        String serialized = Utils.serialize(SampleJson.newInstance().withName("Sample").withCount(123), true);
         assertEquals(readResource("/sampleWithNulls.json"), serialized);
     }
 
     @Test
     public void deserializePolymorphic() throws IOException {
 
+        Utils.unregisterObjectMapperModule(CUSTOM_TYPE_MODULE);
         String json = readResource("/polymorphic.json");
         BaseJson result = Utils.deserialize(json, BaseJson.class);
         assertEquals(result.getClass(), SampleJson.class);
@@ -37,12 +42,10 @@ public class UtilsTest extends BaseTest {
     @Test
     public void deserializePolymorphicWithCustomInjection() throws IOException {
 
-        Utils.registerObjectMapperModule(
-                new SimpleModule().registerSubtypes(new NamedType(org.ohdsi.analysis.pojos.custom.SampleJson.class, "SampleJson"))
-        );
+        Utils.registerObjectMapperModule(CUSTOM_TYPE_MODULE);
         String json = readResource("/polymorphic.json");
         BaseJson result = Utils.deserialize(json, BaseJson.class);
         assertEquals(result.getClass(), org.ohdsi.analysis.pojos.custom.SampleJson.class);
-        assertEquals(((org.ohdsi.analysis.pojos.custom.SampleJson) result).getCustomField(), Long.valueOf(777));
+        assertEquals(((org.ohdsi.analysis.pojos.custom.SampleJson) result).getCustomField(), Integer.valueOf(777));
     }
 }
