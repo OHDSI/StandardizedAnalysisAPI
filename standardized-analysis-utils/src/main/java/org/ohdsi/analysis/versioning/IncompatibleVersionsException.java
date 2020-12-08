2637c4f5d23f3859050d5f7060f7c6b6052ba9a1
anton.abushkevich <anton.abushkevich>
2020-12-08 12:28:24 +0300

    standardized-analysis-utils removed (moved to separate repository) standardized-analysis-specs moved to root

--- standardized-analysis-utils/src/main/java/org/ohdsi/analysis/versioning/IncompatibleVersionsException.java
+++
@@ -1,9 +0,0 @@
-package org.ohdsi.analysis.versioning;
-
-public class IncompatibleVersionsException extends RuntimeException {
-
-    public IncompatibleVersionsException(String message) {
-
-        super(message);
-    }
-}
