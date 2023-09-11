1d7a9225280f112e7b691140088021bbe3b98993
alex-odysseus <alexey.manoylenko@odysseusinc.com>
2023-09-11 13:38:08 +0200

    To proceed with the search in Vocabularies using Apache Solr extraction to a separate library https://github.com/OHDSI/WebAPI/issues/2201 Based on the idea https://github.com/OHDSI/WebAPI/pull/2196 Referencing the latest Standardized Analysis Utils and Circe 1.5.0 release preparation

---
+++ src/main/java/org/ohdsi/vocabulary/SearchProviderConfig.java
@@ -0,0 +1,19 @@
+package org.ohdsi.vocabulary;
+
+public class SearchProviderConfig {
+    private String sourceKey;
+    private String versionKey;
+    
+    public SearchProviderConfig(String sourceKey, String versionKey) {
+        this.sourceKey = sourceKey;
+        this.versionKey = versionKey;
+    }
+    
+    public String getVersionKey() {
+        return versionKey;
+    }
+    
+    public String getSourceKey() {
+        return sourceKey;
+    }
+}
