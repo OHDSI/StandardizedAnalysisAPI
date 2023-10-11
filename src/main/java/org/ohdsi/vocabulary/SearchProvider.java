d05ba526e8cd90a811d0034acadcc588e2208e42
alex-odysseus <alexey.manoylenko@odysseusinc.com>
2023-10-11 09:06:32 +0200

    Advanced Concept Search classes and interfaces (v1.6.0 release preparation)

--- src/main/java/org/ohdsi/vocabulary/SearchProvider.java
+++
@@ -1,9 +0,0 @@
-package org.ohdsi.vocabulary;
-
-import java.util.Collection;
-
-public interface SearchProvider {
-    boolean supports(String vocabularyVersionKey);
-    int getPriority();
-    Collection<Concept> executeSearch(SearchProviderConfig config, String query, String rows) throws Exception;
-}
